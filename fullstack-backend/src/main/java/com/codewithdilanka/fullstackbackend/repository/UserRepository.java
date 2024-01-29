7fa3d0d14f3c67275c8e061d1f6203236e22f5ea
VL.dilankarw <dilankarashmika10@gmail.com>
2024-01-29 23:33:34 +0530

    Update

--- fullstack-backend/src/main/java/com/codewithdilanka/fullstackbackend/repository/UserRepository.java
+++ fullstack-backend/src/main/java/com/codewithdilanka/fullstackbackend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.codewithdilanka.fullstackbackend.repository;
 
-import org.apache.catalina.User;
+//import org.apache.catalina.User;
+import com.codewithdilanka.fullstackbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
